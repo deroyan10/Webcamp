5692638278cecfc035f43c0d86d6bab197331691
EC2 Default User <ec2-user@ip-172-31-46-7.ap-northeast-1.compute.internal>
2024-10-21 08:36:22 +0000

    aboutページの作成

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
   devise_for :users
-
+  root to: "homes#top"
+  get 'homes/about' => 'homes#about', as: 'about'
+  
   resources :books, only: [:new, :index, :show]
   resources :users, only: [:show, :edit]
 end
