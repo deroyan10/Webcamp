e52ff60421e9b2f79b3bb68f54b814854ffdfa60
EC2 Default User <ec2-user@ip-172-31-46-7.ap-northeast-1.compute.internal>
2024-10-26 19:25:23 +0000

    bookのcreate機能の作成

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -3,10 +3,22 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to user_path(current_user.id)
+  end
+
   def index
   end
 
   def show
     @book = Book.new
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :opinion)
   end
 end
