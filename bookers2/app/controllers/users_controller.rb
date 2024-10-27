608f51a93ec137e086eafe2d6c67954906fdd849
EC2 Default User <ec2-user@ip-172-31-46-7.ap-northeast-1.compute.internal>
2024-10-27 17:33:29 +0000

    userのindexページの作成

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @newbook = Book.new
+    @user = User.find(current_user.id)
+    @users = User.all
   end
-  
+
   def show
     @newbook = Book.new
     @books = Book.all
