510da570ff4a1d2a8dd12c51f0452bc924bf8c3d
EC2 Default User <ec2-user@ip-172-31-46-7.ap-northeast-1.compute.internal>
2024-10-27 08:23:01 +0000

    bookのupdate機能の作成

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -18,6 +18,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = User.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path
+  end
 
   def destroy
     book = Book.find(params[:id])
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
 
-  resources :books, only: [:new, :index, :show, :create, :edit, :destroy]
+  resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update]
   resources :users, only: [:show, :edit]
 end
