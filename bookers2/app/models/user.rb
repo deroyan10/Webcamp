a6b0b0bc213abfa77ef6ff8ba74d5d7d0ed66f92
EC2 Default User <ec2-user@ip-172-31-46-7.ap-northeast-1.compute.internal>
2024-10-28 19:12:29 +0000

    User infoの画像表示

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -9,4 +9,12 @@ class User < ApplicationRecord
   
   validates :name, presence: true, length: { minimum: 2, maximum: 20 },uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  
+  def get_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
+  end
 end
