5ee34f8af05a96df209b4d13bfc9e0c61875c73c
EC2 Default User <ec2-user@ip-172-31-46-7.ap-northeast-1.compute.internal>
2024-10-28 13:29:58 +0000

    aboutページをBootstrapで編集

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
   def edit
     book = Book.find(params[:id])
     unless book.user_id == current_user.id
-      redirect_to books_path
+      redirect_to books_pathr
     end
     @book = Book.find(params[:id])
   end
