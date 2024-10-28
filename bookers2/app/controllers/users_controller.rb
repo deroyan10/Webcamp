d0b81a45b4b6d1161febe1b5d656d5b154f6e6a5
EC2 Default User <ec2-user@ip-172-31-46-7.ap-northeast-1.compute.internal>
2024-10-28 08:12:40 +0000

    他人のユーザ情報編集画面に遷移できないようにする

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -12,9 +12,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -24,7 +28,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name, :image, :introduction)
